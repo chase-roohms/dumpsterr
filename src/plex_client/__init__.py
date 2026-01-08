fab0658ed366443c2dbd7cc846e815c3e47fa482
Chase Roohms <chaseroohms@gmail.com>
2026-01-08 17:52:50 -0600

    Implement PlexClient class with methods to retrieve library sections and empty section trash

---
+++ src/plex_client/__init__.py
@@ -0,0 +1,9 @@
+from .plex_client import (
+    get_file_counts,
+    is_valid_directory
+    )
+
+__all__ = [
+    'get_file_counts',
+    'is_valid_directory'
+    ]
---
+++ src/plex_client/plex_client.py
@@ -0,0 +1,37 @@
+import json
+import requests
+from pprint import pp
+
+class PlexClient:
+    def __init__(self, base_url, token):
+        self.base_url = base_url
+        self.token = token
+        self.headers = {
+            'X-Plex-Token': self.token,
+            'Accept': 'application/json'
+        }
+        sections_url = f'{base_url}/library/sections/all'
+
+    def get_library_sections(self):
+        url = f"{self.base_url}/library/sections"
+        response = requests.get(url, headers=self.headers)
+        response.raise_for_status()
+        return { section['title']: section['key'] for section in response.json().get('MediaContainer', {}).get('Directory', []) }
+
+    def empty_section_trash(self, section_key):
+        url = f"{self.base_url}/library/sections/{section_key}/emptyTrash"
+        response = requests.put(url, headers=self.headers)
+        response.raise_for_status()
+        return response.status_code == 200
+
+if __name__ == "__main__":
+    # Example usage / testing
+    import dotenv
+    dotenv.load_dotenv()
+    from os import getenv
+    
+    plex = PlexClient(base_url=getenv('PLEX_URL'), token=getenv('PLEX_TOKEN'))
+    sections = plex.get_library_sections()
+    plex.empty_section_trash(sections['Movies'])
+    plex.empty_section_trash(sections['TV Shows'])
+    pp(sections)
